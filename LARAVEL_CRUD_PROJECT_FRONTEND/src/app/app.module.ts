74c34dfbfcaebfe24580093fb64d78c675905821
chaymae loukili <chaymae@gmail.com>
2023-10-18 10:19:26 +0100

    create components for login and register

--- LARAVEL_CRUD_PROJECT_FRONTEND/src/app/app.module.ts
+++ LARAVEL_CRUD_PROJECT_FRONTEND/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductComponent } from './components/product/product.component';
+import { LoginComponent } from './components/authentication/login/login.component';
+import { RegisterComponent } from './components/authentication/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
